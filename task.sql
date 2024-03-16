24ce45a7848deecb021e98b473e9124163c8aac6
Nour Krichene <krichenenour2@gmail.com>
2024-03-16 10:51:28 +0100

    Add Kubernetes deployment and service files for Adminer, Mariadb, Saray, and Saray UI

--- task.sql
+++ task.sql
@@ -4,14 +4,15 @@ INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('1
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('1e86bb5a-fb79-470f-b166-b53b1a449c06', '2024-03-08 12:54:50.864275', 'efefef', 'tzst', null);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('2b05ba49-d0ea-4b24-849b-1a159caaed54', null, 'Patch is not working', 'Fix Patch', 0);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('305bc353-c7a0-41b7-b9cb-98037886d6f2', '2024-03-08 11:35:00.000000', '', 'Persist data', 0);
-INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('342d9d03-bdea-43b6-811f-e729edb275f6', '2024-03-12 13:34:54.433165', '', 'add number of tasks', 2);
+INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('342d9d03-bdea-43b6-811f-e729edb275f6', '2024-03-12 13:34:54.433165', '', 'add number of tasks', 0);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('42fc2bb5-754b-4e8d-a69b-c75ed189ae5f', '2024-03-10 20:51:56.208164', 'using Github actions and kubernetes', 'Implement ci cd pipeline', 2);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('537afb4f-9a4f-4a0e-afb2-c088be9ae168', '2024-03-08 16:43:06.572297', null, 'Add sign in', 1);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('58a2d42d-5f4a-4c57-8ece-4d09d87e7398', null, 'The creation date must be set automatically', 'Fix creation date problem', 0);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('626ebf90-b3ed-4faa-9606-bf0aeecfa1d9', '2024-03-14 18:09:11.893277', 'add state management with ELF
 https://ngneat.github.io/elf/', 'state management in front app', 1);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('66b88c22-1fec-414e-93e1-e4160cbaf39e', '2024-03-08 11:50:11.412749', '', 'Tast fix creation date', 0);
-INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('71244f18-ce6d-476f-89c1-ec639d6360b0', '2024-03-11 16:13:04.696942', '', 'add switch list', 2);
+INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('71244f18-ce6d-476f-89c1-ec639d6360b0', '2024-03-11 16:13:04.696942', '', 'add switch list', 1);
+INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('72482627-7405-477d-9c9c-94c12a633735', '2024-03-16 11:03:39.648036', 'Figma', 'Transform the design', 1);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('82bd8bbf-1b47-4e41-9acf-70fb370196b0', '2024-03-08 13:04:59.821581', 'change the design of two modals, edit task and add task', 'change the design of modals', 0);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('892443e9-4e3e-47c4-9c77-8aca500c427c', '2024-03-14 18:10:40.501971', '', 'Implement archiving with policy', 1);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('8a5a5057-c050-4bb7-8307-e713a6bf6f50', null, 'Add edit task in ui and backend apps', 'Add edit task', 0);
@@ -36,6 +37,11 @@ INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('c
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('d0fb4dc2-bd2e-42bb-9997-ec5c8f142ea9', null, 'Add the angular app and the spring boot app to docker compose file and test', 'Relaunch the whole app on docker', 0);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('dac988e1-9350-46a3-99d7-0c6ec445bc5f', '2024-03-08 16:43:16.009663', null, 'Add logging', 1);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('dbe8ecbb-34b3-4d05-aa64-4b33117cbf77', null, '', 'Check the necessity of search bar', 0);
-INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('e3bc4f0a-400a-4fb2-92e7-73e6e9f168d3', null, '', 'Add unit test for the spring boot app', 1);
+INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('e3bc4f0a-400a-4fb2-92e7-73e6e9f168d3', null, '-@InjectMocks vs @Mock vs @MockBean
+@SqlGroup({
+        @Sql("classpath:sql/document-verification-test.sql"),
+        @Sql(scripts = "classpath:sql/clean-up-table.sql", executionPhase = AFTER_TEST_METHOD)
+})
+', 'Add unit test for the spring boot app', 1);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('f18592ca-3433-4b74-8c11-cfc71750d4e1', null, 'Add priority field in task model and reorganize tasks by priority', 'Add priority field', 1);
 INSERT INTO saray.task (id, creation_date, description, name, status) VALUES ('f503dff5-7790-4acd-97b5-e8d162931b5d', '2024-03-08 12:59:33.790770', 'ireversible actions must be confirmed', 'Edit design modal confirm before delete', 0);
