d2879e0629fcba883d8637535899804744ce9ef6
Nour Krichene <krichenenour2@gmail.com>
2024-04-01 13:44:09 +0200

    Update task status and priority in integration test SQL script

--- task.sql
+++ task.sql
@@ -1,24 +1,26 @@
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('0ef07ba9-b521-4cab-b2bc-5ab10c9a7b64', '2024-03-17 12:50:42.808546', 'No emty task is possible, fix display when no tasks or just one task, ', 'Add validator in add and edit task', 0, 1);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('42fc2bb5-754b-4e8d-a69b-c75ed189ae5f', '2024-03-10 20:51:56.208164', 'using Github actions and kubernetes', 'Implement ci cd pipeline', 0, 2);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('537afb4f-9a4f-4a0e-afb2-c088be9ae168', '2024-03-08 16:43:06.572297', null, 'Add sign in', 1, 2);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('626ebf90-b3ed-4faa-9606-bf0aeecfa1d9', '2024-03-14 18:09:11.893277', 'add state management with ELF
-https://ngneat.github.io/elf/', 'state management in front app', 0, 0);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('72482627-7405-477d-9c9c-94c12a633735', '2024-03-16 11:03:39.648036', 'Figma,
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('0ef07ba9-b521-4cab-b2bc-5ab10c9a7b64', '2024-03-17 12:50:42.808546', 'No emty task is possible, fix display when no tasks or just one task, ', 'Add validator in add and edit task', 1, 'DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('42fc2bb5-754b-4e8d-a69b-c75ed189ae5f', '2024-03-10 20:51:56.208164', 'using Github actions and kubernetes', 'Implement ci cd pipeline', 2, 'DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('537afb4f-9a4f-4a0e-afb2-c088be9ae168', '2024-03-08 16:43:06.572297', null, 'Add sign in', 1, 'IN_PROGRESS', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('626ebf90-b3ed-4faa-9606-bf0aeecfa1d9', '2024-03-14 18:09:11.893277', 'add state management with ELF
+https://ngneat.github.io/elf/', 'state management in front app', 0, 'DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('72482627-7405-477d-9c9c-94c12a633735', '2024-03-16 11:03:39.648036', 'Figma,
 color fielads in case of no tasks,
-add switch list', 'Transform the design', 1, 4);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('892443e9-4e3e-47c4-9c77-8aca500c427c', '2024-03-14 18:10:40.501971', '', 'Implement archiving with policy', 1, 1);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('8fde1b67-9c00-44f2-8ef3-4f8f2f053517', '2024-03-17 12:47:36.057468', 'all the containers must be deployed in kubernetes cluster', 'Deploy the stack on kubernetes', 1, 5);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('a38d63f1-8d37-4ff7-abf6-f19b0edb798d', null, 'Search how to add a modal that enable to the user the creation of a new task', 'Add create task modal', 0, 3);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('c917ed61-eb6d-47e8-8dbb-a221dff2b428', '2024-03-10 20:51:17.992077', 'https://blog.logrocket.com/angular-unit-testing-tutorial/
+add switch list', 'Transform the design', 6, 'NOT_DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('780fa70d-62bd-4633-ad86-169d3eb63e75', '2024-04-01 13:41:38.008111', '', 'register user via gmail', 0, 'NOT_DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('892443e9-4e3e-47c4-9c77-8aca500c427c', '2024-03-14 18:10:40.501971', '', 'Implement archiving with policy', 4, 'NOT_DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('8fde1b67-9c00-44f2-8ef3-4f8f2f053517', '2024-03-17 12:47:36.057468', 'all the containers must be deployed in kubernetes cluster', 'Deploy the stack on kubernetes', 7, 'NOT_DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('a38d63f1-8d37-4ff7-abf6-f19b0edb798d', '2024-03-14 21:10:40.501000', 'Search how to add a modal that enable to the user the creation of a new task', 'Add create task modal', 3, 'DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('bafd6803-2433-4e72-bf71-584314323ca4', '2024-04-01 13:40:46.010660', '', 'add ssl', 1, 'NOT_DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('c917ed61-eb6d-47e8-8dbb-a221dff2b428', '2024-03-10 20:51:17.992077', 'https://blog.logrocket.com/angular-unit-testing-tutorial/
 
-https://medium.com/swlh/angular-unit-testing-jasmine-karma-step-by-step-e3376d110ab4', 'Add unit test for front app', 2, 1);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('cfec620c-e977-4e62-a225-5af488f210a7', '2024-03-14 18:09:50.173376', '', 'modularize the java app', 1, 0);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('d0fb4dc2-bd2e-42bb-9997-ec5c8f142ea9', null, 'Add the angular app and the spring boot app to docker compose file and test', 'Relaunch the whole app on docker', 0, 4);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('dcbce9f0-7be8-4401-a130-fede3c4bca0f', '2024-03-20 12:11:42.762189', '', 'Add dark mode', 1, 3);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('e3bc4f0a-400a-4fb2-92e7-73e6e9f168d3', null, '-@InjectMocks vs @Mock vs @MockBean
+https://medium.com/swlh/angular-unit-testing-jasmine-karma-step-by-step-e3376d110ab4', 'Add unit test for front app', 1, 'IN_PROGRESS', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('cfec620c-e977-4e62-a225-5af488f210a7', '2024-03-14 18:09:50.173376', '', 'modularize the java app', 2, 'NOT_DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('d0fb4dc2-bd2e-42bb-9997-ec5c8f142ea9', '2024-03-15 18:09:50.173000', 'Add the angular app and the spring boot app to docker compose file and test', 'Relaunch the whole app on docker', 4, 'DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('dcbce9f0-7be8-4401-a130-fede3c4bca0f', '2024-03-20 12:11:42.762189', '', 'Add dark mode', 5, 'NOT_DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('e3bc4f0a-400a-4fb2-92e7-73e6e9f168d3', '2024-03-16 18:09:50.173376', '-@InjectMocks vs @Mock vs @MockBean
 @SqlGroup({
         @Sql("classpath:sql/document-verification-test.sql"),
         @Sql(scripts = "classpath:sql/clean-up-table.sql", executionPhase = AFTER_TEST_METHOD)
 })
-', 'Add unit test for the spring boot app', 2, 0);
-INSERT INTO saray.task (id, creation_date, description, name, status, priority) VALUES ('f18592ca-3433-4b74-8c11-cfc71750d4e1', null, 'Add priority field in task model and reorganize tasks by priority', 'Add priority field', 0, 5);
+', 'Add unit test for the spring boot app', 0, 'IN_PROGRESS', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
+insert into saray.task (id, creation_date, description, name, priority, status, user_id) values ('f18592ca-3433-4b74-8c11-cfc71750d4e1', '2024-03-18 18:09:50.173000', 'Add priority field in task model and reorganize tasks by priority', 'Add priority field', 5, 'DONE', 'f2fbed67-4c34-4d5e-836a-ae52e8908eda');
